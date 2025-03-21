f352d2eb1c76083b0a1954bc07cb82bf97006bf2
Ganesh Patil <ganeshpatil1583@gmail.com>
2025-03-21 15:12:26 +0530

    changed db.js (2.0)

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,8 +4,15 @@ const Schema = mongoose.Schema;
 const ObjectId = mongoose.ObjectId;
 
 const userSchema = new Schema({
-    user_id: {type: String, unique: true},
+    user_id: {type: Number, unique: true},
     username: {type: String, unique: true},
     wallet_address: String,
+    created_at: { type: Number, default: (new Date()).getTime() },
 })
 
+const eventSchema = new Schema({
+    event_id: Number,
+    host_id: {type: mongoose.Schema.Types.Number, ref: "User"},
+    name: String,
+
+})
