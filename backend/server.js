1f55735a22693cc7cd6d40601af8ed9432dd138e
nilu912 <panchalnilu91@gmail.com>
2025-03-21 15:43:07 +0530

    server.js updated and install nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,23 @@
-const expess = require('express')
+const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
 
 dotenv.config()
 
-const app = expess();
-app.use(expess.json())
+// const { usersRouter } = require("./routes/usersRouter")
+const app = express();
+app.use(express.json())
+
+app.get('/', (req,res, next)=>{
+    res.send("Hello")
+})
+// app.use("/api/auth/register", usersRouter)
 
 async function  main() {
-    await mongoose.connect(process.env.MONGO_URL)
+    // await mongoose.connect(process.env.MONGO_URL)
     app.listen(process.env.PORT)
     console.log("Listening on port 5000")
 }
-
-app.get('/', (req,res, next)=>{
-    res.send('Hello')
-})
 
 main()
