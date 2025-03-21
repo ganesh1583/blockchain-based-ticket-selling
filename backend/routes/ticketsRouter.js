bfd287fc8a972a6e3fb5c4de75dadd8fc9b21d50
Ganesh Patil <ganeshpatil1583@gmail.com>
2025-03-21 16:59:17 +0530

    routers formatted

--- backend/routes/ticketsRouter.js
+++ backend/routes/ticketsRouter.js
@@ -3,10 +3,10 @@ const {Router} = require('express');
 const ticketsRouter = Router();
 
 // Buy a ticket (wallet address required, triggers NFT minting)
-ticketsRouter.get("/buy", (req,res,next) => {
+ticketsRouter.get("/buy", (req, res, next) => {
     res.send("Buy a ticket (wallet address required, triggers NFT minting)");
 })
-ticketsRouter.post('/:wallet_address', (req,res,next) => {
+ticketsRouter.post('/:wallet_address', (req, res, next) => {
     const wallet_address = req.params.wallet_address;
     res.send("wallet Address:",wallet_address);
 })
--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -5,16 +5,21 @@ const { userSchema } = require("../db/db");
 const usersRouter = Router();
 
 // Register a new host (username, password, wallet)
-usersRouter.get('/signup', (req,res, next)=>{
+usersRouter.get('/signup', (req, res, next) => {
     res.send("Register");
 })
+
 // Authenticate host with username/password or wallet
-usersRouter.get('/signin', (req,res,next) => {
+usersRouter.get('/signin', (req, res, next) => {
     res.send("Login user")
 })
 // Logout host
-usersRouter.get('/logout', (req,res,next)=>{
+usersRouter.get('/logout', (req, res, next) => {
     res.send("Logout user");
+})
+
+usersRouter.post('/nonce', (req, res, next) => {
+
 })
 
 module.exports = {
