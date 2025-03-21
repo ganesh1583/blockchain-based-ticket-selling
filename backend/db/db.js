8a5ae6d21e97a5b7467f447aef6445418b57d05c
Ganesh Patil <ganeshpatil1583@gmail.com>
2025-03-21 14:49:53 +0530

    changed db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,4 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.ObjectId;
 
-const userSchema = new Schema()
+const userSchema = new Schema({
+    user_id: {type: String, unique: true},
+    username: {type: String, unique: true},
+    wallet_address: String,
+})
+
