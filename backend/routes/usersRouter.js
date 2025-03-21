9c31b63a2c33d68d3baf2a605885a6d1aabf86d9
nilu912 <panchalnilu91@gmail.com>
2025-03-21 17:23:44 +0530

    changes made in react

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -1,29 +1,21 @@
 const { Router } = require('express')
-const { userModel } = require("../db/db");
+const { v4: uuidv4 } = require("uuid");
+
+const { userSchema } = require("../db/db");
 
 const usersRouter = Router();
 
 // Register a new host (username, password, wallet)
-usersRouter.post('/signup', (req,res, next) => {
-    const { username, wallet_address } = req.body;
-    try {
-        await userModel.create({
-            
-        })
-    }
+usersRouter.post('/signup', (req,res, next)=>{
+    res.send("Register");
 })
-
 // Authenticate host with username/password or wallet
 usersRouter.post('/signin', (req,res,next) => {
     res.send("Login user")
 })
 // Logout host
-usersRouter.post('/logout', (req,res,next) => {
+usersRouter.post('/logout', (req,res,next)=>{
     res.send("Logout user");
-})
-
-usersRouter.post('/nonce', (req, res, next) => {
-
 })
 
 module.exports = {
