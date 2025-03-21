a8c676d50848fb856ad6ce8c41df815febcd51ba
nilu912 <panchalnilu91@gmail.com>
2025-03-21 15:31:00 +0530

    Server.js setup

--- backend/server.js
+++ backend/server.js
@@ -4,3 +4,18 @@ const dotenv = require('dotenv')
 const cors = require('cors')
 
 dotenv.config()
+
+const app = expess();
+app.use(expess.json())
+
+async function  main() {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(process.env.PORT)
+    console.log("Listening on port 5000")
+}
+
+app.get('/', (req,res, next)=>{
+    res.send('Hello')
+})
+
+main()
