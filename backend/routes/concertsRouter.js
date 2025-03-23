031a630f2681c79f5a5edec616fd99471e996bcd
nilu912 <panchalnilu91@gmail.com>
2025-03-23 21:52:16 +0530

    concert,user routes and bd modified

--- backend/routes/concertsRouter.js
+++ backend/routes/concertsRouter.js
@@ -1,15 +1,70 @@
 const { Router } = require("express");
-const { eventSchema } = require("../db/db");
+const { eventModel } = require("../db/db");
 
 const concertsRouter = Router();
 
 // Create a new concert (requires authentication)
-concertsRouter.post("/create", (req, res, next) => {
-  res.send("Create concerts");
+concertsRouter.post("/create", async (req, res, next) => {
+  try {
+    const {
+      event_id,
+      name,
+      description,
+      event_date,
+      duration,
+      total_tickets,
+      ticket_price,
+      wallet_address,
+      building,
+      area,
+      city,
+      state,
+      pincode,
+    } = req.body;
+
+    if (
+      !event_id ||
+      !name ||
+      !event_date ||
+      !duration ||
+      !total_tickets ||
+      !ticket_price
+    ) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+    const newEvent = await eventModel.create({
+      event_id,
+      name,
+      description,
+      event_date,
+      duration,
+      total_tickets,
+      ticket_price,
+      wallet_address,
+      venue: {
+        building,
+        area,
+        city,
+        state,
+        pincode,
+      },
+    });
+    res
+      .status(201)
+      .json({ message: "Event created successfully!", event: newEvent });
+  } catch (error) {
+    next(error); // Pass error to Express error handler
+  }
 });
 // Fetch concerts created by the logged-in host
-concertsRouter.get("/my", (req, res, next) => {
-  res.send("My registered concerts");
+concertsRouter.get("/:wallet_address", async (req, res, next) => {
+  const wallet_address = req.params.wallet_address;
+  const eventData = await eventModel.find({
+    walletAddress: wallet_address
+  })
+  if(!eventData)
+    res.status(500).json({message: "No data found!"});
+  res.status(200).json({eventData: eventData})
 });
 // Update concert details
 concertsRouter.put(":conId/update", (req, res, next) => {
@@ -23,8 +78,8 @@ concertsRouter.delete(":conId/delete", (req, res, next) => {
 // fetch all available concerts
 concertsRouter.get("/", (req, res, next) => {
   res.json({
-    message: "You are here"
-  })
+    message: "You are here",
+  });
 });
 concertsRouter.get("/:conId", (req, res, next) => {
   res.send("Fetch details of a specific concert");
